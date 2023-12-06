09e4398c83eb10abfff10cbab1d691d6308d91ee
JuusoVe <juuso.vesanto@gmail.com>
2023-12-06 10:19:13 +0200

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -2,22 +2,5 @@ import { ToriSearchTask } from './types'
 export const TORI_ITEM_SELECTOR = 'a.item_row_flex'
 
 export const SEARCHES: ToriSearchTask[] = [
-    {
-        id: 'trippTrappTuoli',
-        searchUrl:
-            'https://www.tori.fi/uusimaa?q=stokke+tripp+trapp+sy%F6tt%F6tuoli&cg=0&w=1&st=s&st=g&ca=18&l=0&md=th',
-        seenItems: [],
-    },
-    {
-        id: 'tulaExploreReppu',
-        searchUrl:
-            'https://www.tori.fi/uusimaa?q=tula+explore&cg=0&w=1&st=s&st=g&ca=18&l=0&md=th',
-        seenItems: [],
-    },
-    {
-        id: 'babyBjornSitteri',
-        searchUrl:
-            'https://www.tori.fi/uusimaa?q=baby+bj%F6rn+sitteri&cg=0&w=1&st=s&st=g&ca=18&l=0&md=th',
-        seenItems: [],
-    },
+
 ]
